49b00901c354a08f5e9fa3ad88d43cfede20c085
clairxc <clairrrc@gmail.com>
2022-02-08 17:59:10 -0800

    commit-4

--- script.js
+++ script.js
@@ -1,20 +1,72 @@
-// // flip the cards using toggle
-let cards = document.querySelectorAll('.flip-card')
 
+let cards = document.querySelectorAll
+('.flip-card')
+
+let cardFlipped = false
+let card1 = null
+let card2 = null
+// let stopFlip = false
+
+
+// whenever click event is fired, execute flipCard()
 function flipCard() {
-    this.classList.toggle("flip")
-}
-cards.forEach((card) => card.addEventListener("click",flipCard))
+    // console.log('This is working')
+    // console.log(this)
+    // flip the cards using toggle
+    this.classList.toggle('flip')
 
+    // conditions for card flip
+    if (!cardFlipped) {
+        cardFlipped = true
+        card1 = this
+        // console.log(cardFlipped)
+        // console.log(card1)
+    } else {
+        cardFlipped = false
+        card2 = this
+        // console.log(card1)
+        // console.log(card2)
+
+    if (card1 != null && card2 != null) {
+        cards.forEach((card) => {
+            card.removeEventListener('click',flipCard)
+            })
+        } 
+        // need to run check for match
+        setTimeout(checkForMatch, 900)
+
+    }
+}
+// attach eventListener to each card using forEach
+cards.forEach((card) => {
+    card.addEventListener('click',flipCard)
+})
 
 
 // // don't let card flip back once flipped
-function stopFlip() {
-    this.classList.toggle("disabled")
-}
-cards.forEach((card) => card.addEventListener("click", stopFlip))
+// function stopFlip() {
+//     this.classList.toggle('disabled')
+// }
+// cards.forEach((card) => {
+//     card.addEventListener('click', stopFlip)
+// })
+
+// create empty Array to hold two chosen cards
+// within click event (flipCard) push event.target.id for the two cards
+// remove event listener
 
 
+// dont let card flip after two cards have been flipped
+// function stopFlip() {
+//     this.classList.toggle('disabled')
+
+//     let len = cards.length
+//     if (len === 2) {
+//         cards.forEach((card) => {
+//             card.removeEventListener('click',flipCard)
+//         })
+//     }
+// }
 
 
 // // shuffle the cards
@@ -26,96 +78,113 @@ function shuffle() {
 } 
 shuffle()
 
-// // shuffle button shuffles
-let button = document.querySelector('button')
-
-button.addEventListener('click', function() {
-    shuffle(cards)
-}, false)
-
-
-
-
 
 // // let only two cards be flipped over at one time
+// keep track of how many cards have been flipped
+// let flippedCard = false -- move up
+
+// let flipCount = 0
+// function flipCounter () {
+//     if (flipCount < 2) {
+//         flipCount++
+//     }
+// }
 
 
 
+// function flippedCard() {
+//     if (flippedCards[0] === flippedCards[1]) {
+//         console.log('You found match')
+//     } else {
+//         console.log('Sorry, try again')
+//     }
+// }
+// flippedCard()
 
 
 // // see if flipped cards are a match
 // check if cards are match or not
-// try using .isEqualNode()
-// data attributes
+// try using .isEqualNode() or data attributes
+// if using data attributes
+// let cards = document.querySelectorAll('.flip-card')
+
+// cards.dataset.image-- correct
+
+// [cards.index.dataImage]
+
+// const cards = document.querySelectorAll('.flip-card')
+// let card1 = document.querySelector('flip-card').isSameNode
+// let card2 = document.querySelector('flip-card').isSameNode
+// let x = card1.isEqualNode(card2)
+
+// let card1
+// let card2
+
+//need to check if two cards have been clikced before running this function
+
+
+function pauseFlip() {
+    cards.forEach((card) => {
+        card.addEventListener('click',flipCard)
+})
+}
 
 function checkForMatch() {
-    if(cards.dataset.image === cards.dataset.image) {
+    console.log(card1.dataset.image)
+    console.log(card2.dataset.image)
+    if (card1.dataset.image === card2.dataset.image) {
         disableCards()
-        return
+        setTimeout(pauseFlip, 1000)
+    } else {
+        unflipCards()
+        setTimeout(pauseFlip, 1000)
     }
-    unFlipCards()
 }
 
+// stop flip if cards are match
 function disableCards() {
-    cards.removeEventListener('click', flipCard)
-    cards.removeEventListener('click', flipCard)
+    card1.removeEventListener('click', flipCard)
+    card2.removeEventListener('click', flipCard)
+    cardFlipped = false
+    resetGame()
 }
 
+
+// when cards don't match
+// lockBoard = true -- moveup
+
+// unflip cards if no match
 function unflipCards() {
+    stopFlip = true
     setTimeout(() => {
-        this.classList.remove('flip')
-        this.classList.remove('flip')
-    }, 1500)
+        card1.classList.remove('flip')
+        card2.classList.remove('flip')
+        resetGame()
+    }, 900)
 }
 
-// cards.forEach((card) => card.addEventListener('click', flipCard))
 
 
-  
-  // for when cards match
-
-  
-  // for when cards don't match
-
-  
-  // disable cards temporarily
-
-  
-  //enable cards and disable matched cards
+// reset
+function resetGame() {
+    cardFlipped = false
+    stopFlip = false
+    card1 = null
+    card2 = null
+}
 
 
 
 
+// // shuffle button shuffles-- got rid of
+// let button = document.querySelector('button') -- move up
+// let button = document.querySelector('button')
+
+// button.addEventListener('click', function() {
+//     shuffle(cards)
+// }, false)
 
 
-
-// let cardsChosen = []
-// let cardsChosenId = []
-// let cardsWon = []
-
-// function checkForMatch() {
-//     const optionOneId = cardsChosenId[0]
-//     const optionTwoId = cardsChosenId[1]
-
-//     if(optionOneId === optionTwoId) {
-//         cards[optionOneId].setAttribute('src', 'images/akbar.jpg')
-//         cards[optionTwoId].setAttribute('src', 'images/akbar.jpg')
-//         cards[optionOneId].removeEventListener('click', flipCard)
-//         cards[optionTwoId].removeEventListener('click', flipCard)
-//         cardsWon.push(cardsChosen)
-//     } else if (cardsChosen[0] === cardsChosen[1]) {
-//         console.log('You found a match')
-//         cards[optionOneId].setAttribute('src', 'images/boba.jpg')
-//         cards[optionTwoId].setAttribute('src', 'images/boba.jpg')
-//         cards[optionOneId].removeEventListener('click', flipCard)
-//         cards[optionTwoId].removeEventListener('click', flipCard)
-//         cardsWon.push(cardsChosen)
-//     } else {
-//         cards[optionOneId].setAttribute('src', 'images/akbar.jpg')
-//         cards[optionTwoId].setAttribute('src', 'images/akbar.jpg')
-//         alert('Sorry, try again')
-//     }
-// }
 
 // function flippedCard() {
 //     if (cardsInPlay[0] === cardsInPlay[1]) {
